9800c408d72c189b2ab76b98bc601ce532050e53
Nick Morley <nick.morley111@gmail.com>
2019-06-17 17:26:32 +0200

    Add setup.py, requirements, and readme

---
+++ examples/usage.py
@@ -0,0 +1,12 @@
+import visualise_spacy_pattern
+
+# Load the example pattern
+pattern_file = 'examples/pattern_1.json'
+with open(pattern_file) as f:
+    pattern = json.load(f)
+
+graph = visualise_spacy_pattern.to_pydot(pattern)
+png = graph.create_png()
+graph_file = 'examples/graph_1.png'.format()
+with open(graph_file, 'wb') as f:
+    f.write(png)
